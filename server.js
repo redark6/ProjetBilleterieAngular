9b6a7ffac9516c77e2a7511ea770063f836502c7
redark6 <redark66@gmail.com>
2022-03-02 22:03:44 +0100

    lal

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/projet-billeterie-angular'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/projet-billeterie-angular/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/projet-billeterie-angular/src/index.html'));
 });
 app.listen(process.env.PORT || 8080);
